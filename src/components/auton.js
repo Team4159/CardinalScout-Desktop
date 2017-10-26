d112f95cdee818a1e7db857d7ba3b123e638be68
ThisKappaIsGrey <thanatechaumnuaiwit@gmail.com>
2017-10-25 23:00:52 -0700

    the react doesn't work, added eslint

---
+++ src/components/auton.js
@@ -0,0 +1,8 @@
+import React from 'reat';
+
+const Auton = () => (
+    <div>
+        <h1>hello world</h1>
+    </div>
+);
+export default Auton;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import Auton from './containers/auton';
+
+const App = () => (
+    <h1>hello world </h1>
+);
+ReactDOM.render(<App/>, document.getElementById('root'));
